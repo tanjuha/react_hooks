277fc6f45058f9a62ac1ba4f3779d3b35193d5cf
tanjuha <tanya.oleksandrivna19.07@gmail.com>
2019-12-10 18:44:29 +0200

    + alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,22 +1,16 @@
 import React from "react";
 
 export const Alert = ({ alert }) => {
-  if(!alert){
-    return null 
+
+  if(!alert) {
+    return null;
   }
 
   return (
-    <div
-      className={`alert alert-${alert.type || 'worning'} alert-dismissible mt-3`}
-      role="alert"
-    >
+    <div className={`alert alert-${alert.type || "warning"} alert-dismissible`}>
       <strong>Atantion!</strong>
       {alert.text}
-      <button
-        type="button"
-        class="close"
-        aria-label="Close"
-      >
+      <button type="button" className="close" aria-label="Close">
         <span aria-hidden="true">&times;</span>
       </button>
     </div>
